76d5a545ead1c58c18233a7d31159b490dc729f4
Enoch Tam <et.enoch@gmail.com>
2014-02-27 10:53:24 -0500

    translucent nav bar, other small stuff

--- src/com/enochtam/queensmealstatschecker/PreferencesActivity.java
+++ src/com/enochtam/queensmealstatschecker/PreferencesActivity.java
@@ -23,17 +23,6 @@ public class PreferencesActivity extends ActionBarActivity {
 
 
     }
-    /*
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                NavUtils.navigateUpFromSameTask(this);
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
-    */
+
 
 }
