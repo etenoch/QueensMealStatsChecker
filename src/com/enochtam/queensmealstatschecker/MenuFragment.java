e022852773a335eb2a6c58f98f09128bb4003437
Enoch Tam <et.enoch@gmail.com>
2014-03-15 20:08:07 -0400

    Menu UI handler and manue fragment stuff

--- src/com/enochtam/queensmealstatschecker/MenuFragment.java
+++ src/com/enochtam/queensmealstatschecker/MenuFragment.java
@@ -1,6 +1,8 @@
 package com.enochtam.queensmealstatschecker;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,9 +10,23 @@ import android.view.ViewGroup;
 
 public class MenuFragment extends Fragment{
 	
+	MenuUIHandler ui;
+    View rootView;
+    SharedPreferences prefs;
+    View fragView;
+	
     public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
-            // Inflate the layout for this fragment
-            return inflater.inflate(R.layout.activity_menu, container, false);
+		fragView = inflater.inflate(R.layout.activity_menu, container, false); 
+		
+		rootView = fragView;
+		
+		ui = new MenuUIHandler(getActivity(), rootView);
+		
+		prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
+		
+	    setHasOptionsMenu(true);
+
+		return fragView;
     }
 	
 }
---
+++ src/com/enochtam/queensmealstatschecker/MenuUIHandler.java
@@ -0,0 +1,16 @@
+package com.enochtam.queensmealstatschecker;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.view.View;
+
+public class MenuUIHandler {
+	private Context context;
+	
+	public MenuUIHandler(Context context,View rootView){
+		this.context= context;
+		
+	}
+	
+	
+}
